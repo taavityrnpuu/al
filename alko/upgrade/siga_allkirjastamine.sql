ee23093d9a95fc769d7b79d8bbbbafb70238a0c3
Karl <karl@piksel.ee>
2020-09-30 16:28:33 +0300

    confimuudatused live buildiks

--- alko/upgrade/siga_allkirjastamine.sql
+++ alko/upgrade/siga_allkirjastamine.sql
@@ -3,11 +3,11 @@ delete from config WHERE key = 'DIGIDOCSERVICE_URL';
 -- LIVE PARAMEETRID
 insert into config(key, value, type, note) VALUES ('SIGA_URL',			'https://dsig.eesti.ee/', 				'SIGA', 'TEKST. SiGa teenuse url, mille peal toimub digitaalne allkirjastamine.');
 insert into config(key, value, type, note) VALUES ('SIGA_ALGORITHM',	'HmacSHA256', 							'SIGA', 'TEKST. SiGa teenuse tokeni loomise algoritm. Nt: HmacSHA256');
-insert into config(key, value, type, note) VALUES ('SIGA_UUID',			'fd272218-c17a-4b4a-83b8-2ad151578254',	'SIGA', 'TEKST. SiGa teenuse tokeni loomise UUID.');
-insert into config(key, value, type, note) VALUES ('SIGA_SIGNING_KEY',	'dkvwXkhnSEmWqsfJrfGQbqkCcyli5oGv', 	'SIGA', 'TEKST. SiGa teenuse tokeni loomise võti.');
+insert into config(key, value, type, note) VALUES ('SIGA_UUID',			'5cd937eb-4c5d-46d3-b384-984e2f3618a9',	'SIGA', 'TEKST. SiGa teenuse tokeni loomise UUID.');
+insert into config(key, value, type, note) VALUES ('SIGA_SIGNING_KEY',	'qb3WR2nuLGRnlP2cgU8JHNXGMbn84wAB', 	'SIGA', 'TEKST. SiGa teenuse tokeni loomise võti.');
 
 -- TESTI parameetrid
 insert into config(key, value, type, note) VALUES ('SIGA_URL',			'https://dsig-demo.eesti.ee', 			'SIGA', 'TEKST. SiGa teenuse url, mille peal toimub digitaalne allkirjastamine.');
 insert into config(key, value, type, note) VALUES ('SIGA_ALGORITHM',	'HmacSHA256', 							'SIGA', 'TEKST. SiGa teenuse tokeni loomise algoritm. Nt: HmacSHA256');
-insert into config(key, value, type, note) VALUES ('SIGA_UUID',			'8f0cfa04-991d-4858-98f1-a67ab7d0a09d', 'SIGA', 'TEKST. SiGa teenuse tokeni loomise UUID.');
-insert into config(key, value, type, note) VALUES ('SIGA_SIGNING_KEY',	'0PWuebNNWpWq557yICEdejqYQ3IpI33H', 	'SIGA', 'TEKST. SiGa teenuse tokeni loomise võti.');
+insert into config(key, value, type, note) VALUES ('SIGA_UUID',			'fd272218-c17a-4b4a-83b8-2ad151578254', 'SIGA', 'TEKST. SiGa teenuse tokeni loomise UUID.');
+insert into config(key, value, type, note) VALUES ('SIGA_SIGNING_KEY',	'dkvwXkhnSEmWqsfJrfGQbqkCcyli5oGv', 	'SIGA', 'TEKST. SiGa teenuse tokeni loomise võti.');
